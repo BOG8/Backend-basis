d7fcd2b845b4177e635e70401d340cd385424eb5
BOG8 <bolshakov888@gmail.com>
2017-03-09 15:03:43 +0300

    Basic Spring Boot Application

---
+++ src/main/java/ru/bog/Application.java
@@ -0,0 +1,17 @@
+package ru.bog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zac on 09.03.17.
+ */
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
