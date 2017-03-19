878f4408172c5056c8e741aef4d4d819224fa53a
BOG8 <bolshakov888@gmail.com>
2017-03-19 21:34:13 +0300

    Add UserModel

---
+++ src/main/java/ru/bog/UserModel.java
@@ -0,0 +1,12 @@
+package ru.bog;
+
+/**
+ * Created by zac on 19.03.17.
+ */
+
+public class UserModel {
+    private Long id;
+    private String login;
+    private String password;
+    private String email;
+}
